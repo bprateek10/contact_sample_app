8c69cc618e06be899de97182355f10a14cbdb0a1
Prateek Bhatnagar <bprateek10@yahoo.in>
2020-10-03 17:33:17 +0530

    adding locales + coverage report

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    locale = I18n.default_locale
+    locale = params[:locale] if params[:locale].present? && I18n.available_locales.include?(params[:locale].to_sym)
+    I18n.with_locale(locale, &action)
+  end
+
+  def default_url_options
+    { locale: I18n.locale } 
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
   root 'contacts#new'
+  get '/:locale' => 'contacts#new'
+  
+  match '*path' => redirect('/'), via: :get
 
-  resources :contacts, only: [:new, :create]
+  scope "/:locale", locale: /en|ru/ do
+    resources :contacts, only: [:new, :create]
+  end
 end
